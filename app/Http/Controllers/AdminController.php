2a5bb8e88f3d4d780665f858fcaea61ce38af13f
Rohma Rifqi <rohmarifqi31@gmail.com>
2024-02-19 16:58:11 +0700

    push

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,25 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    function index()
+    public function index()
     {
-        return view('admin');
+        $role = Auth::user()->role;
+        $data = User::all(); 
+        return view('admin', compact('role', 'data'));
     }
-    function user()
+
+    public function user()
     {
-        return view('admin');
+        $role = 'User';
+        $data = User::where('role', 'user')->get(); 
+        return view('admin', compact('role', 'data'));
     }
-    function admin()
+
+    public function admin()
     {
-        return view('admin');
+        $role = 'Admin';
+        $data = User::where('role', 'admin')->get(); 
+        return view('admin', compact('role', 'data'));
     }
-    function operator()
+
+    public function operator()
     {
-        return view('admin');
+        $role = 'Operator';
+        $data = User::where('role', 'operator')->get(); 
+        return view('admin', compact('role', 'data'));
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <title>Login</title>
 </head>
+
 <body>
     <div class="container py-5">
         <div class="w-50 center border rounded px-3 py-3 mx-auto">
@@ -36,11 +39,15 @@
                 </div>
             </form>
 
-            <!-- Tambahkan tautan ke halaman registrasi -->
+            <!-- Tambahkan tautan ke halaman registrasi dan login Google -->
             <div class="text-center">
                 <p>Belum punya akun? <a href="{{ route('register') }}">Daftar sekarang</a></p>
+                <p>Login/Register with Google <a href="{{ route('login.google') }}" class="btn btn-google">Login with
+                        Google</a></p>
             </div>
-        </div> 
+
+        </div>
     </div>
 </body>
+
 </html>
